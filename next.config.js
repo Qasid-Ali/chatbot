85ad44cab66ca3e876dbd1d6d650e091265b0f2f
Qasid-Ali <qasidali856@gmail.com>
2023-09-04 18:40:45 +0500

    code refactoring

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:"export",
-  i18n,
-  reactStrictMode: true,
+  output: "export",
+  // i18n,
+  // reactStrictMode: true,
 
   webpack(config, { isServer, dev }) {
     config.experiments = {
